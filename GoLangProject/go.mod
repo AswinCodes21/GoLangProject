50a80058b7ef2867ee97276112028cb7af72a4eb
aswins <aswin.sivakumar@kasadara.com>
2025-03-29 23:20:09 +0530

    Base files commits

---
+++ GoLangProject/go.mod
@@ -0,0 +1,14 @@
+module my_project
+
+go 1.24.1
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+)
---
+++ GoLangProject/pkg/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+var DB *sqlx.DB
+
+func ConnectDB() {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+	var err error
+	DB, err = sqlx.Connect("postgres", connStr)
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+	fmt.Println("Connected to the database")
+}
